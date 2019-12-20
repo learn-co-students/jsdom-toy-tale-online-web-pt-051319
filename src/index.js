c611893f38c643f2f738611c4d62930fd3bb8713
jamievullo <jvullo@protonmail.com>
2019-12-19 19:47:54 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,67 @@
 let addToy = false
 
-document.addEventListener("DOMContentLoaded", ()=>{
-  const addBtn = document.querySelector('#new-toy-btn')
-  const toyForm = document.querySelector('.container')
-  
-    addBtn.addEventListener('click', () => {
-      // hide & seek with the form
-      addToy = !addToy
-      if (addToy) {
-        toyForm.style.display = 'block'
-      } else {
-        toyForm.style.display = 'none'
+document.addEventListener("DOMContentLoaded", () => {
+      getToys();
+      const addBtn = document.querySelector('#new-toy-btn')
+      const toyForm = document.querySelector('.container')
+
+      addBtn.addEventListener('click', () => {
+        // hide & seek with the form
+        addToy = !addToy
+        if (addToy) {
+          toyForm.style.display = 'block'
+        } else {
+          toyForm.style.display = 'none'
+        }
+      })
+      toyForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        postToy(e.target.name.value, e.target.image.value)
+      })
+
+      function getToys() {
+        fetch('http://localhost:3000/toys')
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            data.map(t => renderToy(t))
+            //console.log(data)
+          })
       }
-    })   
-    
-})
-
-function getToys() {
-  fetch("http://localhost:3000/toys") 
-    .then(function(res) {
-      return res.json()
-    }).then(function(json) {
-      parseArray(json)
-    })
-  
-}
-
-function parseArray(json) {
-  console.log(json)
-  
-}
-
-
-// to-do list
-
-// fetch all toys and make cards in class "toy-collection"
-// after page load make GET request to fetch all toy objects
-// h2 tag with toys name
-// img tag with the src of the image attr and class name "toy-avatar"
-// p tag with total likes
-// button tag with a class "like-btn"
-// user clicks add toy. POST request is sent to http://localhost:3000/toys and toy is added
-// user clicks like button. PATCH request to http://localhost:3000/toys/:id updating # of likes
+      
+      function renderToy(toy) {
+      
+        const toyCard = `<div class="card">
+          <h2>${toy.name}</h2>
+          <img src=${toy.image} class="toy-avatar" />
+          <p>${toy.likes}Likes </p>
+          <button class="like-btn">Like <3</button>
+        </div>`
+      
+        const toyBox = document.getElementById('toy-collection')
+        toyBox.innerHTML += toyCard
+      }
+      
+      function postToy(name, url) {
+      
+        fetch('http://localhost:3000/toys', {
+            method: 'POST',
+            headers: {
+      
+              "Content-Type": "application/json",
+              Accept: "application/json"
+            },
+            body: JSON.stringify({
+              "name": name,
+              "image": url,
+              "likes": 0
+            })
+          })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            renderToy(data)
+          })
+      }
